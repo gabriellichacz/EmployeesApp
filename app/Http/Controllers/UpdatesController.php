ce1ac7f0b1c3c45adac4f1f52b7fc4ef582e2bc4
gabriellichacz <gabriellichacz@gmail.com>
2023-12-28 18:07:06 +0100

    update job

--- app/Http/Controllers/UpdatesController.php
+++ app/Http/Controllers/UpdatesController.php
@@ -4,10 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Models\Department;
 use App\Jobs\UpdateEmployees;
-
-use App\Models\Employee;
 use Illuminate\Http\Request;
-use Illuminate\Support\Facades\DB;
 
 class UpdatesController extends Controller
 {
@@ -33,8 +30,68 @@ class UpdatesController extends Controller
         return Department::getDepartmentList();
     }
 
-    public function addUpdateRule()
+    public function addUpdateRule(Request $request)
     {
-        UpdateEmployees::dispatch();
+        $max = null;
+        $min = null;
+        $gender = null;
+        $status = null;
+        $department = null;
+        $updateRule = [];
+
+        if ($request->has('max_salary'))
+        {
+            $max = intval($request->max_salary);
+        }
+
+        if ($request->has('min_salary'))
+        {
+            $min = intval($request->min_salary);
+        }
+
+        if ($request->has('gender'))
+        {
+            $gender = $request->gender;
+        }
+
+        if ($request->has('status'))
+        {
+            $status = $request->status;
+        }
+
+        if ($request->has('department'))
+        {
+            $department = $request->department;
+        }
+
+        if ($request->has('raise_perc'))
+        {
+            $updateRule = [
+                'rule' => 'raise_perc',
+                'field' => 'salary',
+                'value' => $request->raise_perc
+            ];
+        }
+
+        if ($request->has('raise_const'))
+        {
+            $updateRule = [
+                'rule' => 'raise_const',
+                'field' => 'salary',
+                'value' => $request->raise_const
+            ];
+        }
+
+        if ($request->has('job_title'))
+        {
+            $updateRule = [
+                'rule' => 'job_title',
+                'field' => 'title',
+                'value' => $request->job_title
+            ];
+        }
+
+        $updateQueue = new UpdateEmployees($gender, $department, $min, $max, $status, $updateRule);
+        $updateQueue->dispatch();
     }
 }
